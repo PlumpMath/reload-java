120bd9462a56fb2c04c57afe7abf17be04bcd430
Frode Aannevik <frode.aa@gmail.com>
2015-01-16 21:10:55 +0100

    util class for dealing with input/output streams

---
+++ src/main/java/reload/util/IO.java
@@ -0,0 +1,52 @@
+package reload.util;
+
+import java.io.*;
+
+public final class IO {
+
+	private IO() {
+	}
+
+	public static byte[] read( File file ) {
+		try (InputStream fis = new FileInputStream( file )) {
+			return read( fis );
+		} catch( IOException e ) {
+			throw new RuntimeException( e );
+		}
+	}
+
+	public static byte[] read( InputStream is ) {
+		try (BufferedInputStream bis = new BufferedInputStream( is )) {
+			return readAll( bis );
+		} catch( IOException e ) {
+			throw new RuntimeException( e );
+		}
+	}
+
+	private static byte[] readAll( BufferedInputStream bis ) {
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		try {
+			int c;
+			byte[] buff = new byte[8192];
+			while( (c = bis.read( buff, 0, buff.length )) > -1 ) {
+				baos.write( buff, 0, c );
+			}
+		} catch( IOException e ) {
+			throw new RuntimeException( e );
+		}
+		return baos.toByteArray();
+	}
+
+	public static void close( Closeable closeable ) {
+		if( closeable != null ) {
+			try {
+				if( closeable instanceof Flushable ) {
+					((Flushable) closeable).flush();
+				}
+				closeable.close();
+			} catch( IOException e ) {
+			}
+		}
+	}
+
+}
