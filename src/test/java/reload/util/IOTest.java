75461c70b2a7a4abba6c6dad75de93be8ab467ef
Frode Aannevik <frode.aa@gmail.com>
2015-01-16 23:55:18 +0100

    IO: test read bytes from file

--- src/test/java/reload/util/IOTest.java
+++ src/test/java/reload/util/IOTest.java
@@ -1,16 +1,37 @@
 package reload.util;
 
+import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 
 import org.junit.Test;
 
 public class IOTest {
 
 	@Test
+	public void readFileNotFound() {
+
+		try {
+			IO.read( new File( "file_does_not_exist" ) );
+			fail( "Read should fail if file does not exist" );
+		} catch( RuntimeException e ) {
+			assertEquals( "Error", "java.io.FileNotFoundException: "
+					+ "file_does_not_exist (No such file or directory)", e.getMessage() );
+		}
+
+	}
+
+	@Test
+	public void readFile() throws IOException {
+
+		byte[] data = IO.read( new File( getClass().getResource(
+				getClass().getSimpleName() + ".class" ).getFile() ) );
+
+		assertTrue( "no data read from file", data.length > 0 );
+
+	}
+
 	public void closeNull() throws Exception {
 
 		IO.close( null );
